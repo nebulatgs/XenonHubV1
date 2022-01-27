e7eee8880e4ba7cadd97a70b7d1def861716ce8b
DORAN03 Scpf <doran03scpf@gmail.com>
2022-01-27 03:44:32 +0000

    Fixed permissions.

--- permission/GuildPermissionCheckType.java
+++ permission/GuildPermissionCheckType.java
@@ -6,12 +6,12 @@ package com.avairebot.contracts.permission;
  *
  */
 public enum GuildPermissionCheckType {
-    BOT_ADMIN(100, "Bot Developer (Global)", "This permission level gives access to everything regarding the bot. However, the guilds need to allow this with the 'bypass' setting. This only applies to this specific role."),
-    GLOBAL_ADMIN(95, "Global Admin", "This person has the ability to moderate on other guilds. This permission will only be given if all special groups vote for the mod. Otherwise, the MGM permission is applied."),
-    MAIN_GLOBAL_LEADERSHIP(95, "Main Group Leadership (Within :groupId)", "This is the leadership of any group, this depends on the guilds you are in. They have the ability to vote on votes, and are able to assign the HR roles of their own group."),
-    MAIN_GLOBAL_MODERATOR(95, "Main Group Moderators (Within :groupId)", "These are the HR's of a main group, they moderate all discords and can global ban + do everything not group specific on all connected guilds."),
-    LOCAL_GROUP_LEADERSHIP(95, "Admin / Division Leader / Local Admin (Within :localGroupId)", "Bosses of divisional groups. Can assign LGH's"),
-    LOCAL_GROUP_HR(95, "HR / Local Mod (Within :localGroupId)", "The moderation of a group, this is the lowest rank with moderation permissions"),
+    BOT_ADMIN(255, "Bot Developer (Global)", "This permission level gives access to everything regarding the bot. However, the guilds need to allow this with the 'bypass' setting. This only applies to this specific role."),
+    GLOBAL_ADMIN(255, "Global Admin", "This person has the ability to moderate on other guilds. This permission will only be given if all special groups vote for the mod. Otherwise, the MGM permission is applied."),
+    MAIN_GLOBAL_LEADERSHIP(255, "Main Group Leadership (Within :groupId)", "This is the leadership of any group, this depends on the guilds you are in. They have the ability to vote on votes, and are able to assign the HR roles of their own group."),
+    MAIN_GLOBAL_MODERATOR(255, "Main Group Moderators (Within :groupId)", "These are the HR's of a main group, they moderate all discords and can global ban + do everything not group specific on all connected guilds."),
+    LOCAL_GROUP_LEADERSHIP(255, "Admin / Division Leader / Local Admin (Within :localGroupId)", "Bosses of divisional groups. Can assign LGH's"),
+    LOCAL_GROUP_HR(255, "HR / Local Mod (Within :localGroupId)", "The moderation of a group, this is the lowest rank with moderation permissions"),
     GROUP_SHOUT(95, "Group Shout Permission", "Permission to use `!gs`"),
     USER(0, "Regular User", "This is anyone without a permission.");
 
