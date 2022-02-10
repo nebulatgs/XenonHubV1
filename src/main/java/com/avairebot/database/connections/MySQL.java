9cccbaa22dc5610043d433767171128979133ddb
DORAN03 <37197784+DORAN03@users.noreply.github.com>
2022-02-09 19:21:42 -0500

    Update MySQL.java

--- src/main/java/com/avairebot/database/connections/MySQL.java
+++ src/main/java/com/avairebot/database/connections/MySQL.java
@@ -48,7 +48,7 @@ public class MySQL extends HostnameDatabase {
         this(
             dbm,
             dbm.getAvaire().getConfig().getString("database.hostname"),
-            3306,
+            6601,
             dbm.getAvaire().getConfig().getString("database.database"),
             dbm.getAvaire().getConfig().getString("database.username"),
             dbm.getAvaire().getConfig().getString("database.password")
